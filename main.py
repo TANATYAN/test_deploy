d993ecd0e4ff349bf8c6e80efc0a20e798168cc4
TANATYAN <162760955+TANATYAN@users.noreply.github.com>
2024-03-09 09:02:05 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+from typing import Optional
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def hello():
-    return {'Hello': 'World'}
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
